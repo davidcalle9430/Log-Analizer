d62ee60342e651bb50270dd83732a0edafc1a6c5
David Calle <davidcalle9430@gmail.com>
2015-03-21 12:38:03 -0500

    Archivo secuencial terminado con estilo C

--- secuencial.h
+++ secuencial.h
@@ -5,13 +5,12 @@
       	Erika Jeniffer Harker
       	David Calle
      Objetivo:
-      	variables útil prototipo de las funciones que manipulan el log.
+      	 Prototipo de las funciones que manipulan el log.
      Fecha Última Modificación:
       	Marzo 21 de 2015
     */
     #ifndef SECUENCIAL_H
     #define SECUENCIAL_H
-
     int NumeroProcesosUnProcesador(char* nombre_archivo, int total_lineas); ; // prototipo de las funciones
     int NumeroProcesosMasDe64(char* nombre_archivo, int total_lineas);
     void ProcesoUtilizaMasCPU(char *nombre_archivo, int total_lineas, int resultado[3]);
